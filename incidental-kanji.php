160fa2ac536870e884989a0d0f88e1dd966ecd7b
David Besen <dbesen@gmail.com>
2012-10-07 09:11:12 -0600

    Always show form, and print uncommon words

--- incidental-kanji.php
+++ incidental-kanji.php
@@ -8,46 +8,53 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 </head>
 <body>
 <?
-$url = "http://jisho.org/words?jap=*%s*&eng=&dict=edict&common=on";
+$common_url = "http://jisho.org/words?jap=*%s*&eng=&dict=edict&common=on";
+$uncommon_url = "http://jisho.org/words?jap=*%s*&eng=&dict=edict&common=off";
 
-if(!isset($kanji)) {
-        ?><form>Kanji:<input type="text" name="kanji" /><input type="submit"/></form><?
-        exit();
-}
+?><form>Kanji:<input type="text" name="kanji" value="<?=$kanji?>"/><input type="submit"/></form><?
 
-$full_url = sprintf($url, urlencode($kanji));
-$contents = file_get_contents($full_url);
-?><a href="<?=$full_url?>"><?=$full_url?></a><?
+if(!isset($kanji)) exit();
 
-$dom = new DOMDocument;
-@$dom->loadHTML($contents);
-$content = $dom->getElementById("result_content");
-print "<br />";
 print "<pre>";
-print "Kanji: " . $kanji . "<br /><br />";
-$meanings = array();
-foreach($content->getElementsByTagName("tr") as $tr) {
-        $tds = $tr->getElementsByTagName("td");
-        $data = array();
-        foreach($tds as $item) {
-                $data[] = $item->nodeValue;
-        }
-        if(isset($data[2])) {
-                $named_data['kanji'] = trim(utf8_decode($data[0]));
-                $named_data['kana'] = trim(utf8_decode($data[1]));
-                $named_data['meanings'] = $data[2];
-                $meanings[$named_data['kanji']] = $named_data['meanings'];
-                $named_data['kanji'] = str_replace($kanji, "＿", $named_data['kanji']);
-                print $named_data['kanji'] . '[' . $named_data['kana'] . ']   ';
-        }
-}
-
-print "<br /><br /><br />Meanings:<br />";
-
-foreach($meanings as $k=>$meaning) {
-        print "$k: $meaning<br />";
-}
-
+doit($common_url);
+doit($uncommon_url);
 print "</pre>";
+
+function doit($url) {
+        global $kanji;
+        $full_url = sprintf($url, urlencode($kanji));
+        $contents = file_get_contents($full_url);
+        ?><a href="<?=$full_url?>"><?=$full_url?></a><?
+
+        $dom = new DOMDocument;
+        @$dom->loadHTML($contents);
+        $content = $dom->getElementById("result_content");
+        print "<br /><br />";
+        print "Kanji: " . $kanji . "<br /><br />";
+        $meanings = array();
+        foreach($content->getElementsByTagName("tr") as $tr) {
+                $tds = $tr->getElementsByTagName("td");
+                $data = array();
+                foreach($tds as $item) {
+                        $data[] = $item->nodeValue;
+                }
+                if(isset($data[2])) {
+                        $named_data['kanji'] = trim(utf8_decode($data[0]));
+                        $named_data['kana'] = trim(utf8_decode($data[1]));
+                        $named_data['meanings'] = $data[2];
+                        $meanings[$named_data['kanji']] = $named_data['meanings'];
+                        $named_data['kanji'] = str_replace($kanji, "＿", $named_data['kanji']);
+                        print $named_data['kanji'] . '[' . $named_data['kana'] . ']   ';
+                }
+        }
+
+        print "<br /><br /><br />Meanings:<br />";
+
+        foreach($meanings as $k=>$meaning) {
+                print "$k: $meaning<br />";
+        }
+        print "<br /><br /><br />";
+}
+
 ?>
 </body>
