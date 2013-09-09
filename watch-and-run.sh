c9acfeeaeaeba33caefce091137978bfdcd4f52c
David Besen <david.besen@hp.com>
2013-09-09 16:20:35 -0600

    Add watch-and-run.sh

---
+++ watch-and-run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function usage() {
+    echo "Usage: $0 <file/folder to watch> <command to run>"
+    exit 1
+}
+
+if [ -z "$2" ]; then
+    usage
+fi
+
+if [ ! -e "$1" ]; then
+    usage
+fi
+
+while inotifywait -r -e modify -e attrib -e move -e create -e delete "$1"; do
+    $2
+done
