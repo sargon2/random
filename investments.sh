c5ba87dc4af05c1c9696aa6e416f72c72fb2472a
David Besen <david.besen@hp.com>
2013-12-02 12:43:24 -0700

    Fix bitcoin display

--- investments.sh
+++ investments.sh
@@ -7,4 +7,4 @@ curl -s 'http://download.finance.yahoo.com/d/quotes.csv?s=eds&f=l1'
 
 echo -n "Bitcoin: ";
 #curl -s 'https://mtgox.com/api/0/data/ticker.php'
-curl -s 'https://mtgox.com/api/1/BTCUSD/ticker' | tr '}' '\n' | grep last\" | tr ',' '\n' | grep display\" | cut -c 12-18
+curl -s 'https://mtgox.com/api/1/BTCUSD/ticker' | tr '}' '\n' | grep last\" | grep -o "\"display\":\"[^\"]\+" | cut -c 12-
