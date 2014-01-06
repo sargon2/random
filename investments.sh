56120e09b10a11760d8c09c2bb74624751af6836
David Besen <david.besen@hp.com>
2014-01-06 16:12:40 -0700

    Add hpq to inv

--- investments.sh
+++ investments.sh
@@ -5,6 +5,9 @@ curl -s 'http://download.finance.yahoo.com/d/quotes.csv?s=VFINX&f=l1'
 echo -n "NASDAQ:EDS: $";
 curl -s 'http://download.finance.yahoo.com/d/quotes.csv?s=eds&f=l1'
 
+echo -n "HPQ: $";
+curl -s 'http://download.finance.yahoo.com/d/quotes.csv?s=hpq&f=l1'
+
 echo -n "Bitcoin: ";
 #curl -s 'https://mtgox.com/api/0/data/ticker.php'
 curl -s 'https://mtgox.com/api/1/BTCUSD/ticker' | tr '}' '\n' | grep last\" | grep -o "\"display\":\"[^\"]\+" | cut -c 12-
