d7c75fb234dd48f252389f19366d039be2cfa9f5
David Besen <dbesen@gmail.com>
2014-02-04 20:00:15 -0700

    Ignore other-known-chars for the book section

--- aozora/process-aozora.py
+++ aozora/process-aozora.py
@@ -10,18 +10,11 @@ def get_data(filename):
         chars = f.read()
         return set(chars.decode('utf-8').strip())
 
-def read_known_kanji():
-    ret = get_data("known-kanji.txt")
-    ret |= get_data("other-known-chars.txt")
-    return ret
-
 kanji_freq = defaultdict(lambda: 0)
 books = {}
 
-known = read_known_kanji()
-
-def is_known(char):
-    return char in known
+known_kanji = get_data("known-kanji.txt")
+other_known_chars = get_data("other-known-chars.txt")
 
 def process(path):
     with open(path) as f:
@@ -32,12 +25,12 @@ def process(path):
         for item in chars:
             if item.strip() == "":
                 continue
-            if is_known(item):
+            if item in known_kanji:
                 known = known + 1
-                pass
             else:
                 unknown = unknown + 1
-                kanji_freq[item] += 1
+                if item not in other_known_chars:
+                    kanji_freq[item] += 1
         books[path] = 100 * known / (unknown + known)
 
 for (dirpath, dirnames, filenames) in os.walk("textfiles"):
