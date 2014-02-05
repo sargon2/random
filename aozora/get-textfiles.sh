f26c68a9ad244283edad419c25ba736c1db441f7
David Besen <dbesen@gmail.com>
2014-02-04 18:28:42 -0700

    Add get-textfiles.sh

---
+++ aozora/get-textfiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+shopt -s globstar
+
+TARGET_DIR=~/bitbucket/random/aozora/textfiles
+
+mkdir $TARGET_DIR
+
+cd ~/github/aozorabunko
+
+for i in `ls **/*.zip`; do
+    mkdir -p $TARGET_DIR/$i
+    unzip $i '*.txt' -d $TARGET_DIR/$i || :
+done
