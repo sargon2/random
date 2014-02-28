e1f8c53e1a4bf688a984aae17a90e4ca1e945513
David Besen <david.besen@hp.com>
2014-02-28 16:01:44 -0700

    Add some functionality

--- friendly-du/go.py
+++ friendly-du/go.py
@@ -16,6 +16,115 @@ import unittest2
 # Maybe I should have a split() function that takes in a folder, finds the largest unsplit item, and splits it.
 # Then I could just call it N times.
 
+def du_provider(self):
+    pass
+
+def du(num_items, du_provider=du_provider):
+    items = DuResult(du_provider, '.')
+    while(len(items) < num_items):
+        items = items.split()
+    return items.dict()
+
+class DuResult(object):
+    def __init__(self, provider, initial_item = None):
+        self.provider = provider
+        self.sizes_dict = {}
+        if initial_item is not None:
+            self.sizes_dict[initial_item] = provider.get_du(initial_item)
+
+    def __len__(self):
+        return len(self.sizes_dict)
+
+    def dict(self):
+        return self.sizes_dict
+
+    def get_largest(self, dict=None):
+        if dict is None:
+            dict = self.sizes_dict
+        max = None
+        ret = None
+        for key, value in dict.iteritems():
+            if max is None or max < value:
+                ret = key
+        return ret
+
+    def sum(self, dict):
+        return sum(dict.values())
+
+    def split(self):
+        largest = self.get_largest()
+        inner_result = self.provider.get_du_for_children(largest)
+        inner_largest = self.get_largest(inner_result)
+        new_dict = {}
+        inner_largest_size = inner_result[inner_largest]
+        new_dict[inner_largest] = inner_largest_size
+        new_dict[largest + "/<etc>"] = self.sizes_dict[largest] - inner_largest_size
+
+        ret = DuResult(self.provider)
+        ret.sizes_dict = new_dict
+        return ret
+
 class TestDu(unittest2.TestCase):
-    def test_things(self):
-        pass
+    def mock_du_provider(self, mock_fs):
+        class provider(object):
+            def __init__(self, mock_fs):
+                self.mock_fs = mock_fs
+            def get_du(self, path):
+                return self.mock_fs.get(path)
+            def get_du_for_children(self, path):
+                # Ugh.  Mocking.
+                ret = {}
+                for key, value in self.mock_fs.iteritems():
+                    if key == path:
+                        continue
+                    ret[key] = value
+                return ret
+        return provider(mock_fs)
+
+    def assert_result(self, expected_output, num_items, mock_fs):
+        # mock_fs is the simulated result if you run "du -sk <key>"
+        actual_output = du(num_items, du_provider=self.mock_du_provider(mock_fs))
+        self.assertEquals(expected_output, actual_output)
+
+    def test_one_file_total(self):
+        mock_fs = { ".": 30,
+                    "./file1": 26 }
+        self.assert_result({".": 30}, 1, mock_fs)
+
+    def test_one_file_total_fail(self):
+        mock_fs = { ".": 30,
+                    "./file1": 26 }
+        with self.assertRaises(AssertionError):
+            self.assert_result({".": 26}, 1, mock_fs)
+
+    def test_one_file_total_different(self):
+        mock_fs = { ".": 40,
+                    "./file1": 36 }
+        self.assert_result({".": 40}, 1, mock_fs)
+
+    def test_two_files(self):
+        mock_fs = { ".": 44,
+                    "./file1": 20,
+                    "./file2": 20
+                  }
+        self.assert_result({".": 44}, 1, mock_fs)
+
+    def test_two_files_two_returns(self):
+        mock_fs = { ".": 44,
+                    "./file1": 20,
+                    "./file2": 20
+                  }
+        expected_result = { "./file1": 20,
+                            "./<etc>": 24
+                          }
+        self.assert_result(expected_result, 2, mock_fs)
+
+    def test_two_files_two_returns_different(self):
+        mock_fs = { ".": 88,
+                    "./file3": 40,
+                    "./file4": 30
+                  }
+        expected_result = { "./file3": 40,
+                            "./<etc>": 48
+                          }
+        self.assert_result(expected_result, 2, mock_fs)
