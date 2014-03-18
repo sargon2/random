be7de844240057b05c9f34895c4fefb48757e23f
David Besen <david.besen@hp.com>
2014-03-18 13:41:15 -0600

    Comment out mtgox since it's wrong

--- investments.sh
+++ investments.sh
@@ -8,6 +8,6 @@ curl -s 'http://download.finance.yahoo.com/d/quotes.csv?s=eds&f=l1'
 echo -n "HPQ: $";
 curl -s 'http://download.finance.yahoo.com/d/quotes.csv?s=hpq&f=l1'
 
-echo -n "Bitcoin: ";
+#echo -n "Bitcoin: ";
 #curl -s 'https://mtgox.com/api/0/data/ticker.php'
-curl -s 'https://mtgox.com/api/1/BTCUSD/ticker' | tr '}' '\n' | grep last\" | grep -o "\"display\":\"[^\"]\+" | cut -c 12-
+#curl -s 'https://mtgox.com/api/1/BTCUSD/ticker' | tr '}' '\n' | grep last\" | grep -o "\"display\":\"[^\"]\+" | cut -c 12-
