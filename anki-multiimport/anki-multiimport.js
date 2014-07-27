c3474db8307c584db65d9f87f95433cc27e4d93c
David Besen <dbesen@gmail.com>
2014-07-27 11:42:36 -0600

    Add and remove sentences and words

--- anki-multiimport/anki-multiimport.js
+++ anki-multiimport/anki-multiimport.js
@@ -1,22 +1,86 @@
 var sentenceIndex = 0;
+var wordIndex = 0;
+
+function addinput(div, title) {
+    div.innerHTML += title + ": ";
+    input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    div.appendChild(input);
+}
+
 function addsentence() {
-    d = document.createElement('div');
-    d.setAttribute('id', sentenceIndex);
+    sentenceDiv = document.createElement('div');
+    sentenceDiv.setAttribute('id', "sentence" + sentenceIndex);
+    sentenceDiv.innerHTML += "<hr/>";
 
-    i = document.createElement('input');
-    i.setAttribute('type', 'text');
-    d.appendChild(i);
+    removeSentenceButton = document.createElement('button');
+    removeSentenceButton.setAttribute('onclick', 'removesentence(' + sentenceIndex + ')');
+    removeSentenceButton.setAttribute('type', 'button');
+    removeSentenceButton.innerHTML = "remove sentence";
+    sentenceDiv.appendChild(removeSentenceButton);
 
-    r = document.createElement('button');
-    r.setAttribute('onclick', 'removesentence(' + sentenceIndex + ')');
-    r.setAttribute('type', 'button');
-    r.innerHTML = "remove sentence";
+    sentenceDiv.innerHTML += "<br />";
 
-    d.appendChild(r);
-    document.getElementById("myForm").appendChild(d);
+    addinput(sentenceDiv, "Expression");
+    sentenceDiv.innerHTML += "<br />";
+
+    addinput(sentenceDiv, "Meaning");
+    sentenceDiv.innerHTML += "<br />";
+
+    addinput(sentenceDiv, "Reading");
+    sentenceDiv.innerHTML += "<br />";
+
+    addinput(sentenceDiv, "Audio");
+    sentenceDiv.innerHTML += "<br />";
+
+    addWordButton = document.createElement('button');
+    addWordButton.setAttribute('onclick', 'addword(' + sentenceIndex + ')');
+    addWordButton.setAttribute('type', 'button');
+    addWordButton.innerHTML = "Add word";
+    sentenceDiv.appendChild(addWordButton);
+
+    document.getElementById("myForm").appendChild(sentenceDiv);
     sentenceIndex++;
 }
 
+function addword(sentenceIndex) {
+    wordDiv = document.createElement('div');
+    wordDiv.setAttribute('id', "word" + wordIndex);
+
+    addinput(wordDiv, "Word");
+    wordDiv.innerHTML += "<br />";
+
+    addinput(wordDiv, "Meaning");
+    wordDiv.innerHTML += "<br />";
+
+    addinput(wordDiv, "Context");
+    wordDiv.innerHTML += "<br />";
+
+    addinput(wordDiv, "Reading");
+    wordDiv.innerHTML += "<br />";
+
+    removeWordButton = document.createElement('button');
+    removeWordButton.setAttribute('onclick', 'removeword(' + wordIndex + ')');
+    removeWordButton.setAttribute('type', 'button');
+    removeWordButton.innerHTML = "remove word";
+    wordDiv.appendChild(removeWordButton);
+
+    sentenceDiv = document.getElementById('sentence' + sentenceIndex);
+    sentenceDiv.appendChild(wordDiv);
+    wordIndex++;
+}
+
+function removeItem(id) {
+    div = document.getElementById(id);
+    div.parentNode.removeChild(div);
+}
+
+function removeword(wordIndex) {
+    // TODO: "are you sure?"
+    removeItem('word' + wordIndex);
+}
+
 function removesentence(sentenceIndex) {
-    document.getElementById("myForm").removeChild(document.getElementById(sentenceIndex));
+    // TODO: "are you sure?"
+    removeItem('sentence' + sentenceIndex);
 }
