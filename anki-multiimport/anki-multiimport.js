6ea44159e0a1fce3a741815a6dccdcd78de87c92
David Besen <dbesen@gmail.com>
2014-07-27 12:10:50 -0600

    Add sentence cards

--- anki-multiimport/anki-multiimport.js
+++ anki-multiimport/anki-multiimport.js
@@ -5,6 +5,8 @@ function addinput(div, title) {
     div.innerHTML += title + ": ";
     input = document.createElement('input');
     input.setAttribute('type', 'text');
+    // TODO: better layout
+    input.setAttribute('style', 'width: 80%');
     div.appendChild(input);
 }
 
@@ -78,9 +80,46 @@ function removeItem(id) {
 function removeword(wordIndex) {
     // TODO: "are you sure?"
     removeItem('word' + wordIndex);
+    onFormChange();
 }
 
 function removesentence(sentenceIndex) {
     // TODO: "are you sure?"
     removeItem('sentence' + sentenceIndex);
+    onFormChange();
+}
+
+function onFormChange() {
+    output = document.getElementById('output');
+    output.innerHTML = "";
+    // for each sentence
+    var nodes = document.getElementById('myForm').childNodes;
+    for(var i=0;i<nodes.length;i++) {
+        sentenceDiv = nodes[i];
+        if(sentenceDiv.nodeType != 1) continue;
+        addSentence(sentenceDiv);
+    }
+}
+
+function addSentence(sentenceDiv) {
+    // for each input
+    inputs = sentenceDiv.getElementsByTagName('input');
+    expression = inputs[0].value;
+    meaning = inputs[1].value;
+    reading = inputs[2].value;
+    audio = inputs[3].value;
+
+    output.innerHTML += "a one piece of info::c writing and reading -> meaning (sentences)";
+    output.innerHTML += "\t";
+    output.innerHTML += "Japanese stroke order and reading -> meaning (sentences)";
+    output.innerHTML += "\t";
+    output.innerHTML += "0";
+    output.innerHTML += "\t";
+    output.innerHTML += expression + "\t" + meaning + "\t" + reading + "\t" + audio;
+    output.innerHTML += "\n";
+
+    // for each word div
+    // get inputs
+    // make word card
+    // make kanji cards, if not kana
 }
