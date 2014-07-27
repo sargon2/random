d235569e5b7e096f8a074a9ee97d55fadf4f9027
David Besen <dbesen@gmail.com>
2014-07-27 11:21:26 -0600

    Add and remove sentence text boxes

--- anki-multiimport/anki-multiimport.js
+++ anki-multiimport/anki-multiimport.js
@@ -1,2 +1,22 @@
+var sentenceIndex = 0;
+function addsentence() {
+    d = document.createElement('div');
+    d.setAttribute('id', sentenceIndex);
 
-alert("hi");
+    i = document.createElement('input');
+    i.setAttribute('type', 'text');
+    d.appendChild(i);
+
+    r = document.createElement('button');
+    r.setAttribute('onclick', 'removesentence(' + sentenceIndex + ')');
+    r.setAttribute('type', 'button');
+    r.innerHTML = "remove sentence";
+
+    d.appendChild(r);
+    document.getElementById("myForm").appendChild(d);
+    sentenceIndex++;
+}
+
+function removesentence(sentenceIndex) {
+    document.getElementById("myForm").removeChild(document.getElementById(sentenceIndex));
+}
