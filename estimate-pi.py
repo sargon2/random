3d6364ba54130353d9c15affc1696864921e18bc
David Besen <david.besen@hp.com>
2015-01-14 15:25:47 -0700

    Speed up estimate-pi.py

--- estimate-pi.py
+++ estimate-pi.py
@@ -5,19 +5,24 @@ from __future__ import division
 import math
 import sys
 
-numerator = 1
-denominator = 1
-best_error = 9999
+def go():
 
-while(True):
-    result = numerator / denominator
-    error = abs(math.pi - result)
-    if error < best_error:
-        best_error = error
-        print str(numerator) + " / " + str(denominator) + " = " + str(result) + ", error = " + str(error)
-    if str(result) == str(math.pi):
-        sys.exit(0)
-    if result < math.pi :
-        numerator = numerator + 1
-    elif result > math.pi :
-        denominator = denominator + 1
+    numerator = 1
+    denominator = 1
+    best_error = 9999
+
+    while(True):
+        result = numerator / denominator
+        error = abs(math.pi - result)
+        if error < best_error:
+            best_error = error
+            print str(numerator) + " / " + str(denominator) + " = " + str(result) + ", error = " + str(error)
+        if error < .00000000001:
+            sys.exit(0)
+        if result < math.pi :
+            numerator = numerator + 1
+        elif result > math.pi :
+            denominator = denominator + 1
+
+if __name__ == "__main__":
+    go()
