598e40fb761e67c2335190e55eaf85787673fb53
David Besen <david.besen@hp.com>
2015-02-20 15:16:56 -0700

    Get tdd quine to pass

--- tdd/quine/quine.py
+++ tdd/quine/quine.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 import sys
 header = '#!/usr/bin/python\nimport sys\nheader = '
-footer = '\nsys.stdout.write(header + repr(header) + repr(footer) + footer)'
+footer = '\nsys.stdout.write(header + repr(header) + "\\nfooter = " + repr(footer) + footer)\n'
 sys.stdout.write(header + repr(header) + "\nfooter = " + repr(footer) + footer)
-
