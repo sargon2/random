e24d616c1565933010cc5704f7a4787eec4e0e6f
David Besen <david.besen@hp.com>
2015-02-27 14:07:56 -0700

    work on final-goal...

--- tdd/final-goal/go.py
+++ tdd/final-goal/go.py
@@ -9,9 +9,6 @@ class Game(App):
 
 
 class TestGame(unittest2.TestCase):
-    def isProfitable(self, app):
-        pass
-
     def getGoodGame(self):
         return Game()
 
@@ -22,10 +19,17 @@ class TestGame(unittest2.TestCase):
         return App()
 
     def assertGameIsFun(self, game):
-        pass
+        self.assertHasPleasingColorScheme(game)
+        self.assertIsJuicy(game)
+        # what else? assert that it has addictive elements?
+        # assert when the user runs it, it might go better the following time they run it?
 
     def assertAppIsProfitable(self, app):
-        self.assertTrue(self.isProfitable(app))
+        self.assertHasMechanismToCollectMoney(app)
+        self.assertIsPopular(app)
+        # now what? assert a certain % of users provide money?
+        # assert it's been used to collect at least $x?
+        # assert that it's been used to collect at least $x in the last y time?
 
     def test_game_is_successful(self):
         game = self.getGoodGame()
