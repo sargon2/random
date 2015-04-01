ede2a604028e3616cf5d05274060595e763f3488
David Besen <david.besen@hp.com>
2015-04-01 09:55:24 -0600

    use * in two-arg method

--- tdd/treat_scalars_and_arrays_the_same/go.py
+++ tdd/treat_scalars_and_arrays_the_same/go.py
@@ -29,7 +29,7 @@ class TestSomething(unittest2.TestCase):
     @aggregate_arg(summer, 1)
     @aggregate_arg(summer, 2)
     def two_arg_method(self, arg1, arg2):
-        return arg1 + arg2
+        return arg1 * arg2
 
     def test_one_arg(self):
         # The idea here is if method is defined the same, method(list) should iterate and method(arg) should run once.
@@ -38,7 +38,7 @@ class TestSomething(unittest2.TestCase):
 
     def test_two_args(self):
         # But then what about 2-arg methods?  method(list, list) should run list*list times.
-        self.assertEquals(1 + 2, self.two_arg_method(1, 2))
-        self.assertEquals(1 + 3 + 2 + 3, self.two_arg_method([1, 2], 3))
-        self.assertEquals(1+3+2+3+2+3+2+4, self.two_arg_method([1, 2], [3, 4]))
-        self.assertEquals(1+2+1+3, self.two_arg_method(1, [2, 3]))
+        self.assertEquals(1*2, self.two_arg_method(1, 2))
+        self.assertEquals(1*3+2*3, self.two_arg_method([1, 2], 3))
+        self.assertEquals(1*3+2*3+1*4+2*4, self.two_arg_method([1, 2], [3, 4]))
+        self.assertEquals(1*2+1*3, self.two_arg_method(1, [2, 3]))
