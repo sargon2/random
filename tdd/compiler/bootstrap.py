ee1ee33e8986ede47d23b975d9168bf796a301af
David Besen <david.besen@hp.com>
2015-05-15 13:40:13 -0600

    Add bootstrap tests to ensure we can run multiple statements

--- tdd/compiler/bootstrap.py
+++ tdd/compiler/bootstrap.py
@@ -28,4 +28,7 @@ class TestCompilerBootstrap(unittest2.TestCase):
 
     def test_assert_equals(self):
         self.assertEquals(0, compile_and_run("assertEquals(1, 1)"))
+        self.assertEquals(0, compile_and_run("assertEquals(1, 1)\nassertEquals(2, 2)"))
+        self.assertNotEquals(0, compile_and_run("assertEquals(1, 2)\nassertEquals(2, 2)"))
+        self.assertNotEquals(0, compile_and_run("assertEquals(1, 1)\nassertEquals(1, 2)"))
         self.assertNotEquals(0, compile_and_run("assertEquals(1, 2)"))
