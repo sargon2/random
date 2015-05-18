66232c77b713cc0dcc3cee57bb9cb9d787e93647
David Besen <david.besen@hp.com>
2015-05-18 14:25:38 -0600

    Make assert a function

--- tdd/compiler/bootstrap.py
+++ tdd/compiler/bootstrap.py
@@ -27,8 +27,8 @@ class TestCompilerBootstrap(unittest2.TestCase):
                 pass
 
     def test_assert_equals(self):
-        self.assertEquals(0, compile_and_run("assertEquals(1, 1);"))
-        self.assertEquals(0, compile_and_run("assertEquals(1, 1);\nassertEquals(2, 2);"))
-        self.assertNotEquals(0, compile_and_run("assertEquals(1, 2);\nassertEquals(2, 2);"))
-        self.assertNotEquals(0, compile_and_run("assertEquals(1, 1);\nassertEquals(1, 2);"))
-        self.assertNotEquals(0, compile_and_run("assertEquals(1, 2);"))
+        self.assertEquals(0, compile_and_run("assert(equals(1, 1));"))
+        self.assertEquals(0, compile_and_run("assert(equals(1, 1));\nassert(equals(2, 2));"))
+        self.assertNotEquals(0, compile_and_run("assert(equals(1, 2));\nassert(equals(2, 2));"))
+        self.assertNotEquals(0, compile_and_run("assert(equals(1, 1));\nassert(equals(1, 2));"))
+        self.assertNotEquals(0, compile_and_run("assert(equals(1, 2));"))
