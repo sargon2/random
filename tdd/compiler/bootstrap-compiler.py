4d7c550af0276d6132a6f8162cc365c1a774cd4a
David Besen <dbesen@gmail.com>
2015-05-20 20:53:55 -0600

    Start writing real bootstrap compiler (breaks tests, needs cleanup)

--- tdd/compiler/bootstrap-compiler.py
+++ tdd/compiler/bootstrap-compiler.py
@@ -2,33 +2,148 @@
 
 import sys
 import os
+import re
 
-infile = sys.argv[3]
 outfile = sys.argv[2]
+infile = sys.argv[3]
 
 with open(infile) as f:
     contents = f.read()
 
-# Note that contents is always the same.  So we could just hardcode the output if we wished.
-# But, that would increase duplication (since both this script and the hardcoded output would need argv assignment)
-# So that's why we read and process infile directly here.
+# assignment = variable "=" value ";"
 
-# TODO: this stuff is dup'd with compiler-source.newlang; how do I remove that duplication?
-# This one compiles to python and the other could compile to asm or whatever.
-# This only has to handle the compiler's code itself; other code (that the compiler handles) isn't important here.
-new_contents = "#!/usr/bin/env python\nimport sys\n"
-for line in contents.splitlines():
+class ParseWrong(BaseException):
+    pass
 
-    if line == "chmod(outfile, 0755);":
-        line = "import os\nos.chmod(outfile, 0755)"
-    if line == "contents = read_file(infile);":
-        line = "with open(infile) as f:\n    contents = f.read()\n"
-    if line == "write_file(outfile, new_contents);":
-        line = "with open(outfile, \"w\") as f:\n    f.write(new_contents)\n"
+def try_consume(token, regex):
+    if re.match(regex, token):
+        return token
+    raise ParseWrong("Expected " + regex + ", got " + token) # TODO: can we avoid the raise?
 
-    new_contents += line + "\n"
+def assignment(tokens):
+    variable = try_consume(tokens[0], "[a-z]+")
+    tokens = tokens[1:]
+
+    try_consume(tokens[0], "=")
+    tokens = tokens[1:]
+
+    tokens = statement(tokens)
+
+    return tokens
+
+def arg_list(tokens):
+    arg = try_consume(tokens[0], "[a-z]+")
+    tokens = tokens[1:]
+
+    done = False
+    while not done:
+        try:
+            tokens = remaining_arg(tokens)
+        except ParseWrong:
+            done = True
+        except:
+            raise
+    return tokens
+
+def remaining_arg(tokens):
+    try_consume(tokens[0], ",")
+    tokens = tokens[1:]
+
+    arg = try_consume(tokens[0], "[a-z]+")
+    tokens = tokens[1:]
+
+    return tokens
+
+def function_definition(tokens):
+    fn_name = try_consume(tokens[0], "[a-z]+")
+    tokens = tokens[1:]
+
+    try_consume(tokens[0], "=")
+    tokens = tokens[1:]
+
+    try_consume(tokens[0], "\(")
+    tokens = tokens[1:]
+
+    tokens = arg_list(tokens)
+
+    try_consume(tokens[0], "\)")
+    tokens = tokens[1:]
+
+    try_consume(tokens[0], "{")
+    tokens = tokens[1:]
+
+    tokens = statements(tokens)
+
+    try_consume(tokens[0], "}")
+    tokens = tokens[1:]
+
+    return tokens
+
+def function_invocation(tokens):
+    fn_name = try_consume(tokens[0], "[a-z]+")
+    tokens = tokens[1:]
+
+    try_consume(tokens[0], "\(")
+    tokens = tokens[1:]
+
+    tokens = arg_list(tokens)
+
+    try_consume(tokens[0], "\)")
+    tokens = tokens[1:]
+
+    return tokens
+
+def statement(tokens):
+    try:
+        tokens = function_definition(tokens)
+    except ParseWrong:
+        try:
+            tokens = function_invocation(tokens)
+        except ParseWrong:
+            try:
+                tokens = assignment(tokens)
+            except ParseWrong:
+                try:
+                    try_consume(tokens[0], "[a-z\[\]]+")
+                    tokens = tokens[1:]
+                except ParseWrong:
+                    raise
+
+    return tokens
+
+
+def statements(tokens):
+    done = False
+    while not done:
+        if tokens:
+            try:
+                tokens = statement(tokens)
+                try_consume(tokens[0], ";")
+                tokens = tokens[1:]
+
+            except ParseWrong:
+                done = True
+            except:
+                raise
+        else:
+            done = True
+    return tokens
+
+def remove_comments(string):
+    return re.sub('#.*', '', string)
+
+contents = remove_comments(contents)
+
+tokens = re.findall("[a-z0-9\[\]_]+|=|;|\(|\)|,|{|}|`", contents)
+try:
+    tokens = statements(tokens)
+except ParseWrong as e:
+    print "Parse error:", e
+else:
+    if tokens:
+        print "Parse error: tokens left", tokens
 
 with open(outfile, "w") as f:
-    f.write(new_contents)
+    f.write("")
 
 os.chmod(outfile, 0755)
