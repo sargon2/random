c6256d56ef4241518cf20db63a9128ee8a93903e
David Besen <dbesen@gmail.com>
2015-05-21 20:14:12 -0600

    Move more towards declarative

--- tdd/compiler/bootstrap-compiler.py
+++ tdd/compiler/bootstrap-compiler.py
@@ -34,6 +34,7 @@ def backtrack(fn):
         return result
     return wrapper
 
+@backtrack
 def Or(*args):
     for item in args:
         r = item()
@@ -41,6 +42,7 @@ def Or(*args):
             return r
     return False
 
+@backtrack
 def Each(*args):
     ret = []
     for item in args:
@@ -61,35 +63,26 @@ def ZeroOrMore(arg):
             done = True
     return ret
 
-word = lambda: try_consume("[a-z\[\]]+") # TODO: lambda and try_consume are dup'd
-equals = lambda: try_consume("=")
-comma = lambda: try_consume(",")
-open_paren = lambda: try_consume("\(")
-close_paren = lambda: try_consume("\)")
-open_brace = lambda: try_consume("{")
-close_brace = lambda: try_consume("}")
-semicolon = lambda: try_consume(";")
+def Literal(regex):
+    return lambda: try_consume(regex)
 
+word = Literal("[a-z\[\]]+") # TODO: "Literal" dup'd
+equals = Literal("=")
+comma = Literal(",")
+open_paren = Literal("\(")
+close_paren = Literal("\)")
+open_brace = Literal("{")
+close_brace = Literal("}")
+semicolon = Literal(";")
+
+# TODO: too many lambda keywords...
 assignment = lambda: Each(word, equals, lambda: Or(statement, word))
-
 remaining_arg = lambda: Each(comma, word)
-
 arg_list = lambda: Each(word, lambda: ZeroOrMore(remaining_arg))
-
-@backtrack # TODO: get rid of decorator
-def function_definition():
-    return Each(word, equals, open_paren, arg_list, close_paren, open_brace, statements, close_brace)
-
-@backtrack
-def function_invocation():
-    return Each(word, open_paren, arg_list, close_paren)
-
-@backtrack
-def statement():
-    return Or(function_definition, function_invocation, assignment)
-
+function_definition = lambda: Each(word, equals, open_paren, arg_list, close_paren, open_brace, statements, close_brace)
+function_invocation = lambda: Each(word, open_paren, arg_list, close_paren)
+statement = lambda: Or(function_definition, function_invocation, assignment)
 statement_with_semi = lambda: Each(statement, semicolon)
-
 statements = lambda: ZeroOrMore(statement_with_semi)
 
 
@@ -98,7 +91,7 @@ def remove_comments(string):
 
 contents = remove_comments(contents)
 
-tokens = re.findall("[a-z0-9\[\]_]+|=|;|\(|\)|,|{|}|`", contents)
+tokens = re.findall("[a-z0-9\[\]_]+|=|;|\(|\)|,|{|}|`", contents) # TODO: dup'd with literals above
 position = 0
 parsed = statements()
 if position < len(tokens):
