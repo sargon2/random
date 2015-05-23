627b5caf63f3004bd7551bb88315d9ceafae1a9d
David Besen <dbesen@gmail.com>
2015-05-23 17:47:54 -0600

    remove some of the parse and tocode methods

--- tdd/compiler/bootstrap-compiler.py
+++ tdd/compiler/bootstrap-compiler.py
@@ -17,8 +17,6 @@ indent = ""
 class SingleToken(object):
     def __init__(self, result):
         self.result = result
-    def parse(self):
-        return self.result
     def tocode(self):
         if self.result == "args[2]":
             return "sys.argv[2]"
@@ -65,7 +63,7 @@ class Or(object):
     @backtrack
     def parse(self):
         for item in self.args:
-            r = item().parse()
+            r = parse(item())
             if not is_bad_parse(r):
                 self.result = r
                 return r
@@ -79,7 +77,7 @@ class Each(object):
     def parse(self):
         ret = []
         for item in self.args:
-            r = item().parse()
+            r = parse(item())
             if is_bad_parse(r):
                 return r
             ret.append(r)
@@ -92,7 +90,7 @@ class ZeroOrOne(object):
         self.arg = arg
 
     def parse(self):
-        r = self.arg().parse()
+        r = parse(self.arg())
         if is_bad_parse(r):
             self.result = []
             return []
@@ -108,7 +106,7 @@ class ZeroOrMore(object):
         done = False
         ret = []
         while not done:
-            r = self.arg().parse()
+            r = parse(self.arg())
             if not is_bad_parse(r):
                 ret.append(r)
             else:
@@ -121,14 +119,16 @@ class OneOrMore(object):
         self.arg = arg
 
     def parse(self):
-        return Each(self.arg, lambda: ZeroOrMore(self.arg)).parse()
+        return parse(Each(self.arg, lambda: ZeroOrMore(self.arg)))
 
 class literal(object):
     def __init__(self, regex):
         self.regex = regex
 
     def parse(self):
-        return try_consume(self.regex)
+        self.result = try_consume(self.regex)
+        return self.result
+
 
 # TODO: "lambda" dup'd
 word = lambda: literal("[a-z\[\]]+") # TODO: "literal" dup'd
@@ -142,7 +142,10 @@ semicolon = lambda: literal(";")
 backtick = lambda: literal("`[^`]+`");
 
 def parse(ob):
-    ob.result = ob.defn().parse()
+    if hasattr(ob, 'defn'): # TODO: reflection is bad..
+        ob.result = parse(ob.defn())
+    else:
+        ob.result = ob.parse()
     if is_bad_parse(ob.result):
         return None
     return ob
@@ -153,44 +156,26 @@ class assignment(object):
     def defn(self):
         return Each(word, equals, lambda: Or(statement, word)) # TODO: 'lambda' shouldn't be there
 
-    def parse(self): # TODO: boilerplate
-        r = parse(self)
-        return r
-
     def tocode(self):
-        return indent + tocode(self.result[0]) + " = " + tocode(self.result[2])
+        # TODO: result.result is weird
+        return indent + tocode(self.result.result[0]) + " = " + tocode(self.result.result[2])
 
 class remaining_arg(object):
     def defn(self):
         return Each(comma, word)
 
-    def parse(self):
-        return parse(self)
-
-    def tocode(self): # TODO: lots of these dup'd
-        return tocode(self.result)
-
 class arg_list(object):
     def defn(self):
         return ZeroOrOne(lambda: Each(word, lambda: ZeroOrMore(remaining_arg)))
-
-    def parse(self):
-        return parse(self)
-
-    def tocode(self):
-        return tocode(self.result)
 
 class function_definition(object):
     def defn(self):
         return Each(word, equals, open_paren, arg_list, close_paren, open_brace, statements, close_brace)
 
-    def parse(self):
-        return parse(self)
-
     def tocode(self):
         global indent
         indent += "    "
-        ret = "def " + tocode(self.result[0]) + "(" + tocode(self.result[3]) + "):\n" + tocode(self.result[6]) + indent + "pass"
+        ret = "def " + tocode(self.result.result[0]) + "(" + tocode(self.result.result[3]) + "):\n" + tocode(self.result.result[6]) + indent + "pass"
         indent = indent[4:]
         return ret
 
@@ -198,18 +183,12 @@ class function_invocation(object):
     def defn(self):
         return Each(word, open_paren, arg_list, close_paren)
 
-    def parse(self):
-        return parse(self)
-
     def tocode(self):
         return indent + tocode(self.result) # TODO: statement should be the only thing with indent on it...
 
 class invoke_system(object):
     def defn(self):
         return backtick()
-
-    def parse(self):
-        return parse(self)
 
     def tocode(self):
         global indent
@@ -223,43 +202,23 @@ class statement(object):
     def defn(self):
         return Or(function_definition, function_invocation, assignment, invoke_system)
 
-    def parse(self):
-        return parse(self)
-
     def tocode(self):
-        r = tocode(self.result)
-        return r
+        return tocode(self.result)
 
 class statement_with_semi(object):
     def defn(self):
         return Each(statement, semicolon)
 
-    def parse(self):
-        r = parse(self)
-        return r
-
     def tocode(self):
-        r = tocode(self.result[0]) + "\n" # strip semis
-        return r
+        return tocode(self.result.result[0]) + "\n" # strip semis
 
 class statements(object):
     def defn(self):
         return ZeroOrMore(statement_with_semi)
 
-    def parse(self):
-        r = parse(self)
-        return r
-
-    def tocode(self):
-        r = tocode(self.result)
-        return r
-
 class program(object):
     def defn(self):
         return statements()
-
-    def parse(self):
-        return parse(self)
 
     def tocode(self):
         ret = "#!/usr/bin/env python\nimport sys\nimport os\n"
@@ -278,7 +237,9 @@ def tocode(element):
         for item in element:
             ret += tocode(item)
         return ret
-    return element.tocode()
+    if hasattr(element, 'tocode'): # TODO: reflection is bad
+        return element.tocode()
+    return tocode(element.result)
 
 contents = remove_comments(contents)
 
