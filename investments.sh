bfc2ac1a7c03dd87ed0d065dd5fd5244a38889bc
David Besen <david.besen@hp.com>
2015-08-11 16:50:53 -0600

    Add quotes around l_fix

--- investments.sh
+++ investments.sh
@@ -2,4 +2,4 @@
 
 STOCKS=vfinx,vbk,hpq
 
-curl -s "http://finance.google.com/finance/info?q=$STOCKS" | egrep "\"t\"|l_fix" | paste -s -d' \n' | sed 's/,\"t\" : //' | sed 's/ ,\"l_fix\" : /: \$/' | tr -d '\"'
+curl -s "http://finance.google.com/finance/info?q=$STOCKS" | egrep "\"t\"|\"l_fix\"" | paste -s -d' \n' | sed 's/,\"t\" : //' | sed 's/ ,\"l_fix\" : /: \$/' | tr -d '\"'
