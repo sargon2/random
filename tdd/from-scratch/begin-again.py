df90511177f0e914ad055e1750cad0cb11e80ccc
David Besen <david.besen@hp.com>
2016-06-16 10:43:33 -0600

    Cleanup in begin-again

--- tdd/from-scratch/begin-again.py
+++ tdd/from-scratch/begin-again.py
@@ -4,9 +4,13 @@ def assert_true(item):
     with should_fail():
         assert_false(item)
 
+def fail():
+    with should_fail():
+        pass
+
 def assert_false(item):
     if item:
-        raise AssertionError(str(item) + " was True, expected False")
+        fail()
 
 class should_fail():
     def __enter__(self):
@@ -16,15 +20,15 @@ class should_fail():
         if type is AssertionError:
             return True
         # This raise is untested because it's impossible to test it without duplication.
+        # That means it's really configuration -- we're configuring what to do when a test fails.
         raise AssertionError("Expected a failure")
 
 def assert_equals(a, b):
-    with should_fail():
-        assert_not_equals(a, b)
+    assert_true(a == b)
 
 def assert_not_equals(a, b):
-    if a == b:
-        raise AssertionError(str(a) + " == " + str(b))
+    with should_fail():
+        assert_equals(a, b)
 
 assert_true(True)
 assert_false(False)
@@ -36,8 +40,7 @@ with should_fail():
     assert_false(True)
 
 with should_fail():
-    with should_fail():
-        pass
+    fail()
 
 assert_equals(1, 1)
 with should_fail():
@@ -46,3 +49,5 @@ with should_fail():
 assert_not_equals(1, 2)
 with should_fail():
     assert_not_equals(1, 1)
+
+print "pass"
