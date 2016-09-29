7222384623a5114effb3a9dd2963a00896b78a44
David Besen <dbesen@gmail.com>
2016-09-29 09:46:25 -0600

    Add demo for e-voting fill in

---
+++ e-voting/voting-fill-in-demo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import random
+import time
+
+total_votes = 0
+ary = []
+while True:
+    r = random.randint(0, total_votes + 30)
+    ary = ary + [" "] * (r - len(ary) + 1)
+    if ary[r] == " ":
+        total_votes += 1
+        if bool(random.getrandbits(1)):
+            ary[r] = "A"
+        else:
+            ary[r] = "B"
+        print "".join(ary)
+        time.sleep(0.1)
