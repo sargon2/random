b48eb137b54774d049528749fbdd2b41ca9b3eff
David Besen <dbesen@gmail.com>
2017-10-20 12:14:40 -0600

    Shortest-first mu puzzle search

--- mu-puzzle.py
+++ mu-puzzle.py
@@ -25,28 +25,51 @@ def children(s):
         if s[i:i+2] == "UU":
             yield s[0:i] + s[i+2:]
 
-def dfs(max_depth, state = None):
-    if state == None:
-        state = start_state
+def sfs(): # shortest-first search
+    state = start_state
+    unexplored = [state]
+    explored = []
 
-    if state == end_state:
-        print(state)
-        return "Win"
+    i = 0
+    while(True):
+        explored.append(state)
+        if(i % 1000) == 0:
+            print("i is", i, "state is", state)
+        i += 1
+        unexplored.extend(children(state))
+        unexplored = list(set(unexplored) - set(explored))
+        unexplored.sort(key = len)
 
-    if max_depth == 0:
-        return
+        state = unexplored.pop(0)
+        if state == end_state:
+            print("Win")
+            sys.exit(0)
 
-    for child in children(state):
-        result = dfs(max_depth-1, child)
-        if result == "Win":
-            print(state)
-            return "Win"
+sfs()
 
-# Iterative deepening search
-max_depth = 1
-while True:
-    max_depth += 1
-    print("Depth", max_depth)
-    result = dfs(max_depth)
-    if result == "Win":
-        sys.exit()
+
+#def dfs(max_depth, state = None):
+#    if state == None:
+#        state = start_state
+#
+#    if state == end_state:
+#        print(state)
+#        return "Win"
+#
+#    if max_depth == 0:
+#        return
+#
+#    for child in children(state):
+#        result = dfs(max_depth-1, child)
+#        if result == "Win":
+#            print(state)
+#            return "Win"
+#
+## Iterative deepening search
+#max_depth = 1
+#while True:
+#    max_depth += 1
+#    print("Depth", max_depth)
+#    result = dfs(max_depth)
+#    if result == "Win":
+#        sys.exit()
