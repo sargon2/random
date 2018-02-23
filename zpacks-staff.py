bacfd17ec84426b38f1033de585b254b728e2d75
David Besen <dbesen@gmail.com>
2018-02-23 11:14:51 -0700

    Work on zpacks-staff.py

--- zpacks-staff.py
+++ zpacks-staff.py
@@ -6,11 +6,9 @@
 # - No two combinations of pole lengths should be the same total length
 
 def what():
-    best = None
-    best_val = None
     for i in range(0, 595, 5):
-        for j in range(0, 595, 5):
-            for k in range(0, 595, 5):
+        for j in range(0, i, 5):
+            for k in range(0, j, 5):
                 l = 595 - (i+j+k)
                 if l < 0:
                     continue
@@ -31,18 +29,24 @@ def what():
                 sizeset.add(i+k+l)
                 sizeset.add(j+k+l)
                 sizeset.add(i+j+k+l)
-                if len(sizeset) <= 12:
-                    continue
+                #if len(sizeset) <= 12:
+                #    continue
                 #if 380 not in sizeset:
                 #    continue
-                if 480 not in sizeset:
+                if 480 not in sizeset and 485 not in sizeset and 475 not in sizeset:
                     continue
-                if 520 not in sizeset:
+                if 520 not in sizeset and 525 not in sizeset and 515 not in sizeset:
                     continue
-                if best is None or best_val > max(i,j,k,l):
-                    best = (i,j,k,l)
-                    best_val = max(i,j,k,l)
-    print(best)
+                # The two smallest plus 2.5 should equal the middle + 2.5 should equal the largest.
+                sizes = sorted([i, j, k, l])
+                for m in range(20, 35, 5):
+                    if abs(sizes[2] + m - sizes[3]) > 5:
+                        continue
+                    if abs(sizes[0] + sizes[1] + m - sizes[3]) > 5:
+                        continue
+
+                    print(m, sizes)
+
 
 if __name__ == "__main__":
     what()
