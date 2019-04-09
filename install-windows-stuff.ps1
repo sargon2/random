f0f210d8dcf20196ad2b609d19ebe429e8fc37a2
David Besen <dbesen@gmail.com>
2019-04-09 13:28:38 -0600

    Add windows install script

---
+++ install-windows-stuff.ps1
@@ -0,0 +1,18 @@
+# Self-elevate to administrator
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-noexit -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+choco upgrade -y 7zip
+choco upgrade -y putty
+choco upgrade -y googlechrome
+choco upgrade -y dropbox
+choco upgrade -y notepadplusplus
+choco upgrade -y steam
+choco upgrade -y launchy
+choco upgrade -y anki
+choco upgrade -y paint.net
+choco upgrade -y audacity
+choco upgrade -y vlc
+choco upgrade -y minecraft
+choco upgrade -y spacesniffer
