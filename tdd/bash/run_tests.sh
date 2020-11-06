eaec0d006b1db8f58896857a2350f528ea75109f
David Besen <dbesen@gmail.com>
2020-11-06 14:31:33 -0700

    Combine into run_tests.sh

--- tdd/bash/run_tests.sh
+++ tdd/bash/run_tests.sh
@@ -1,11 +1,68 @@
 #!/bin/bash -e
 
-. ./asserts.sh
+function equals {
+    if [ "$1" == "$2" ]; then
+        return 0
+    fi
+    return 1
+}
+
+function assertEquals {
+    assertFails assertNotEquals $1 $2
+}
+
+function assertNotEquals {
+    if equals $1 $2; then
+        return 1
+    fi
+}
+
+function assertFails {
+    local RET=0
+    "$@" || RET=$?
+    assertNotEquals $RET 0
+}
+
+function internal_test_asserts {
+
+    assertEquals true true
+    assertEquals false false
+
+    assertFails assertEquals true false
+    assertFails assertEquals false true
+
+    assertFails assertFails assertEquals true true
+    assertFails assertFails assertEquals false false
+
+    assertFails assertFails assertFails assertEquals true false
+    assertFails assertFails assertFails assertEquals false true
+
+    assertNotEquals true false
+    assertNotEquals false true
+
+    assertFails assertNotEquals true true
+    assertFails assertNotEquals false false
+
+
+    assertEquals "a" "a"
+    assertNotEquals "a" "b"
+    assertNotEquals "a" 1
+    assertEquals "asdf" "asdf"
+    assertEquals "1" "1"
+    assertEquals "3" "3"
+}
 
 functions=$(declare -F | cut -d" " -f3-)
 
 overall_passed=true
 for func in $functions; do
+    if [[ $func = internal_test_* ]]; then
+        # Silently run internal tests
+        # If we do $func || RET=$? here, it causes the tests to false negative.
+        # That's super annoying and needs a fix, so we can output a reasonable
+        # message on internal failure.
+        $func
+    fi
     if [[ $func = test_* ]]; then
         RET=0
         $func || RET=$?
