54c68e6e1f623670fd45d632f5e52b5f9edd7098
David Besen <dbesen@gmail.com>
2022-05-28 19:51:17 -0600

    Make estimate-pi run longer

--- estimate-pi.py
+++ estimate-pi.py
@@ -8,15 +8,17 @@ def go():
     numerator = 1
     denominator = 1
     best_error = 9999
+    result = 0
 
     while(True):
+        prev_result = result
         result = numerator / denominator
+        if prev_result == result:
+            sys.exit(0)
         error = abs(math.pi - result)
         if error < best_error:
             best_error = error
             print(str(numerator) + " / " + str(denominator) + " = " + str(result) + ", error = " + str(error))
-        if error < .00000000001:
-            sys.exit(0)
         if result < math.pi :
             numerator = numerator + 1
         elif result > math.pi :
