7a563a8f2895c53da3f422b49ff7045a06de9a14
David Besen <dbesen@gmail.com>
2022-07-08 17:52:15 -0600

    Update windows installer

--- install-windows-stuff.ps1
+++ install-windows-stuff.ps1
@@ -9,11 +9,14 @@ choco upgrade -y googlechrome
 choco upgrade -y dropbox
 choco upgrade -y notepadplusplus
 choco upgrade -y steam
-choco upgrade -y launchy
-choco upgrade -y anki
+# choco upgrade -y launchy
+# choco upgrade -y anki
 choco upgrade -y paint.net
 choco upgrade -y audacity
-choco upgrade -y vlc
-choco upgrade -y minecraft
-choco upgrade -y spacesniffer
-choco upgrade -y veracrypt
+# choco upgrade -y vlc
+choco upgrade -y minecraft-launcher
+# choco upgrade -y spacesniffer
+# choco upgrade -y veracrypt
+choco upgrade -y discord
+
+wsl.exe --install
