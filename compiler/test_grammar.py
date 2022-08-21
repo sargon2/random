44e13c666fff5a68ccd690baf41071bda88407d1
David Besen <dbesen@gmail.com>
2022-08-20 20:14:37 -0600

    Add tests for OneOrMore

--- compiler/test_grammar.py
+++ compiler/test_grammar.py
@@ -1,6 +1,6 @@
 import unittest
 
-from grammar import Regex, ZeroOrMore, ZeroOrOne
+from grammar import OneOrMore, Regex, ZeroOrMore, ZeroOrOne
 
 class TestGrammar(unittest.TestCase):
     def test_zeroorone_zero(self):
@@ -107,3 +107,65 @@ class TestGrammar(unittest.TestCase):
         self.assertEqual("b", result[0][1].literal_value)
         self.assertEqual("a", result[1][0].literal_value)
         self.assertEqual("b", result[1][1].literal_value)
+
+    def test_oneormore_one(self):
+        z = OneOrMore("a")
+        class grammar_provider:
+            a = Regex("a")
+        code_provider = None
+        result = z.parse("a", grammar_provider, code_provider)
+        self.assertEqual(1, len(result))
+        self.assertEqual("a", result[0][0].literal_value) # two zeroes because the first one is overall group and the second is item within the group
+
+    def test_oneormore_two(self):
+        z = OneOrMore("a")
+        class grammar_provider:
+            a = Regex("a")
+        code_provider = None
+        result = z.parse("aa", grammar_provider, code_provider)
+        self.assertEqual(2, len(result))
+        self.assertEqual("a", result[0][0].literal_value)
+        self.assertEqual("a", result[1][0].literal_value)
+
+    def test_oneormore_zero(self):
+        z = OneOrMore("a")
+        class grammar_provider:
+            a = Regex("a")
+        code_provider = None
+        result = z.parse("", grammar_provider, code_provider)
+        self.assertIsNone(result) # Parse error
+
+    def test_oneormore_zero_two_items(self):
+        z = OneOrMore("a", "b")
+        class grammar_provider:
+            a = Regex("a")
+            b = Regex("b")
+        code_provider = None
+        result = z.parse("", grammar_provider, code_provider)
+        self.assertIsNone(result) # Parse error
+
+    def test_oneormore_one_two_items(self):
+        z = OneOrMore("a", "b")
+        class grammar_provider:
+            a = Regex("a")
+            b = Regex("b")
+        code_provider = None
+        result = z.parse("ab", grammar_provider, code_provider)
+        self.assertEqual(1, len(result))
+        self.assertEqual("a", result[0][0].literal_value)
+        self.assertEqual("b", result[0][1].literal_value)
+
+    def test_oneormore_two_two_items(self):
+        z = OneOrMore("a", "b")
+        class grammar_provider:
+            a = Regex("a")
+            b = Regex("b")
+        code_provider = None
+        result = z.parse("abab", grammar_provider, code_provider)
+        self.assertEqual(2, len(result))
+        self.assertEqual("a", result[0][0].literal_value)
+        self.assertEqual("b", result[0][1].literal_value)
+        self.assertEqual("a", result[1][0].literal_value)
+        self.assertEqual("b", result[1][1].literal_value)
+
+    # TODO tests for Each
