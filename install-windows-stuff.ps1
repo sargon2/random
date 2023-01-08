a8c369fa5e7c9e1337cee940161d26cfc5a5aa45
David Besen <dbesen@gmail.com>
2023-01-08 09:03:41 -0700

    Update chocolatey install command

--- install-windows-stuff.ps1
+++ install-windows-stuff.ps1
@@ -1,7 +1,7 @@
 # Self-elevate to administrator
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-noexit -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco upgrade -y 7zip
 choco upgrade -y putty
