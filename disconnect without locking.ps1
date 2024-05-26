e6b8d765600f48407ca700131e54829f0e5aa69a
David Besen <dbesen@gmail.com>
2024-05-26 09:40:35 -0600

    Add disconnect without locking.ps1

---
+++ disconnect without locking.ps1
@@ -0,0 +1,15 @@
+# Useful for unlocking a PC remotely to run a steam game remotely.  You must log in, then run this script to disconnect, which will leave the PC unlocked for Steam to use.
+
+# https://stackoverflow.com/a/64576446
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+ if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+  Exit
+ }
+}
+
+# Remainder of script here
+
+tscon 1 /dest:console
