fec13fa31d8762e85c0e15082e6400c8b68b7759
David Besen <dbesen@gmail.com>
2025-01-30 09:10:26 -0700

    Add dropbox_systemd

---
+++ dropbox_systemd/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash -ex
+
+mkdir -p ~/.config/systemd/user/
+
+cp dropbox.service ~/.config/systemd/user/
+
+systemctl --user daemon-reload
+systemctl --user enable dropbox
+systemctl --user start dropbox
