25f564ab1a3d0ccaccd0d06874d6d0430c7f5d79
David Besen <dbesen@gmail.com>
2025-02-17 09:27:17 -0700

    Wait less in wait-for-dropbox-sync

--- wait-for-dropbox-sync.sh
+++ wait-for-dropbox-sync.sh
@@ -7,7 +7,8 @@ unset DISPLAY # otherwise dropbox tries to use X
 ~/bin/dropbox lansync n # apparently makes it use less network bandwidth
 
 # wait until done syncing
-# TODO: we're doing extra status calls here...
 # TODO: dup'd with reinstall-dropbox.sh
-while ! ~/bin/dropbox status 2>&1 | grep -q "Up to date"; do sleep 5; ~/bin/dropbox status; sleep 5; done
+while ! ~/bin/dropbox status 2>&1 | grep -q "Up to date"; do
+    sleep 5
+done
 
