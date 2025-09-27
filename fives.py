3bd205adf1a5ccfac28bd11f2912eff4a09244e3
David Besen <dbesen@gmail.com>
2025-09-26 18:42:31 -0600

    Add parens to fives.py

--- fives.py
+++ fives.py
@@ -5,18 +5,37 @@ results = {}
 def is_whole(n):
     return n == int(n)
 
-def doit(string_so_far):
-    result = eval(string_so_far)
-    if result > 0 and is_whole(result):
-        result = int(result)
-        if not result in results or len(string_so_far) < len(results[result]):
-            results[result] = string_so_far
-    if len(string_so_far) > 17:
+def num_fives(s):
+    return s.count("5")
+
+def doit(string_so_far, num_parens=0):
+    if num_parens < 0:
         return
-    doit(string_so_far + "*5")
-    doit(string_so_far + "+5")
-    doit(string_so_far + "-5")
-    doit(string_so_far + "/5")
+    if num_parens == 0:
+        try:
+            result = eval(string_so_far)
+        except:
+            return
+        if result > 0 and is_whole(result):
+            result = int(result)
+            if not result in results or num_fives(string_so_far) < num_fives(results[result]):
+                results[result] = string_so_far
+    if len(string_so_far) > 15:
+        return
+    doit(string_so_far + "*5", num_parens)
+    doit(string_so_far + "+5", num_parens)
+    doit(string_so_far + "-5", num_parens)
+    doit(string_so_far + "/5", num_parens)
+
+    doit(string_so_far + ")*5", num_parens-1)
+    doit(string_so_far + ")+5", num_parens-1)
+    doit(string_so_far + ")-5", num_parens-1)
+    doit(string_so_far + ")/5", num_parens-1)
+
+    doit(string_so_far + "*(5", num_parens+1)
+    doit(string_so_far + "+(5", num_parens+1)
+    doit(string_so_far + "-(5", num_parens+1)
+    doit(string_so_far + "/(5", num_parens+1)
 
 doit("5")
 
