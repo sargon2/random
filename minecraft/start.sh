829e1c93072f150823f75a17d6c3cf2bf34d9163
David Besen <dbesen@gmail.com>
2026-06-07 17:35:02 -0600

    Make start.sh run as minecraft user

--- minecraft/start.sh
+++ minecraft/start.sh
@@ -1,10 +1,30 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+set -euo pipefail
 
 cd "$(dirname "$0")"
 
-export MINECRAFT_UID=`id -u`
-export MINECRAFT_GID=`id -g`
+MINECRAFT_USER="minecraft"
+MINECRAFT_DATA_ROOT="/srv/minecraft"
+MINECRAFT_DATA_LOCATION="/srv/minecraft/server1"
 
-export MINECRAFT_DATA_LOCATION="/srv/minecraft/server1"
+if ! id "$MINECRAFT_USER" >/dev/null 2>&1; then
+  sudo useradd --system --create-home --shell /usr/sbin/nologin "$MINECRAFT_USER"
+fi
 
-docker compose up
+sudo install -d \
+  -o "$MINECRAFT_USER" \
+  -g "$MINECRAFT_USER" \
+  -m 750 \
+  "$MINECRAFT_DATA_ROOT"
+
+sudo install -d \
+  -o "$MINECRAFT_USER" \
+  -g "$MINECRAFT_USER" \
+  -m 750 \
+  "$MINECRAFT_DATA_LOCATION"
+
+export MINECRAFT_UID="$(id -u "$MINECRAFT_USER")"
+export MINECRAFT_GID="$(id -g "$MINECRAFT_USER")"
+export MINECRAFT_DATA_LOCATION
+
+docker compose up -d
