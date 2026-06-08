d56042f4446519a330628b7d230c49ab85ff0224
David Besen <dbesen@gmail.com>
2026-06-08 00:12:18 -0600

    Add attach.sh

---
+++ minecraft/attach.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+MINECRAFT_USER="minecraft"
+MINECRAFT_DATA_ROOT="/srv/minecraft"
+MINECRAFT_DATA_LOCATION="/srv/minecraft/server1"
+
+export MINECRAFT_UID="$(id -u "$MINECRAFT_USER")"
+export MINECRAFT_GID="$(id -g "$MINECRAFT_USER")"
+export MINECRAFT_DATA_LOCATION
+
+echo "Detach with ^p follwed by ^q."
+
+docker compose attach minecraft
