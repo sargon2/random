26e6f92246a0889a101910d5e3e4c0ab064c2f90
David Besen <dbesen@gmail.com>
2026-06-08 00:13:53 -0600

    Make attach show logs

--- minecraft/attach.sh
+++ minecraft/attach.sh
@@ -11,6 +11,8 @@ export MINECRAFT_UID="$(id -u "$MINECRAFT_USER")"
 export MINECRAFT_GID="$(id -g "$MINECRAFT_USER")"
 export MINECRAFT_DATA_LOCATION
 
-echo "Detach with ^p follwed by ^q."
+docker compose logs
+
+echo "(Detach with ^p follwed by ^q.)"
 
 docker compose attach minecraft
