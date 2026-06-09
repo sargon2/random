dca73b1b407453bb9f543de223ba4b9860004591
David Besen <dbesen@gmail.com>
2026-06-09 11:49:04 -0600

    Update make_backup to run as root

--- minecraft/make_backup.sh
+++ minecraft/make_backup.sh
@@ -1,16 +1,28 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
+date
+
+MINECRAFT_USER=minecraft
+DROPBOX_USER=besen
+SRC_PATH=/srv/minecraft
+DST_PATH=/home/besen/Dropbox/minecraft/metal/
+FILENAME=minecraft.tar.gz
 TMP_DIR=$(mktemp -d)
-DESTINATION=$HOME/Dropbox/minecraft/metal/
+DROPBOX_WAITER=/home/besen/github/sargon2/random/bin/wait-for-dropbox-sync
 
-mkdir -p $DESTINATION
+if (( $EUID != 0 )); then
+    echo "Error: Please run this script as root." >&2
+    exit 1
+fi
+
+sudo -u $DROPBOX_USER mkdir -p $DST_PATH
 
 pushd /srv
-sudo tar cvfz $TMP_DIR/minecraft.tar.gz minecraft/
+tar cvf - minecraft/ | gzip -9 > $TMP_DIR/$FILENAME
 popd
-sudo chown $USER:$USER $TMP_DIR/minecraft.tar.gz
-mv -f $TMP_DIR/minecraft.tar.gz $DESTINATION
+sudo chown $DROPBOX_USER:$DROPBOX_USER $TMP_DIR/$FILENAME
+mv -f $TMP_DIR/$FILENAME $DST_PATH
 
 rm -rf $TMP_DIR
 
-wait-for-dropbox-sync
+sudo -u $DROPBOX_USER $DROPBOX_WAITER
