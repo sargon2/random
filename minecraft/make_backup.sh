e40b49ff976402c962d8fbb160fc938047c5a54d
David Besen <dbesen@gmail.com>
2026-06-20 11:17:19 -0600

    Move minecraft backups to Google Drive

--- minecraft/make_backup.sh
+++ minecraft/make_backup.sh
@@ -3,26 +3,25 @@
 date
 
 MINECRAFT_USER=minecraft
-DROPBOX_USER=besen
-SRC_PATH=/srv/minecraft
-DST_PATH=/home/besen/Dropbox/minecraft/metal/
+CLOUD_USER=besen
+DST_PATH="/home/besen/Google Drive/backups/minecraft/metal/"
 FILENAME=minecraft.tar.gz
 TMP_DIR=$(mktemp -d)
-DROPBOX_WAITER=/home/besen/github/sargon2/random/bin/wait-for-dropbox-sync
+CLOUD_WAITER="/home/besen/github/sargon2/random/bin/wait-rclone-vfs.sh"
 
 if (( $EUID != 0 )); then
     echo "Error: Please run this script as root." >&2
     exit 1
 fi
 
-sudo -u $DROPBOX_USER mkdir -p $DST_PATH
+sudo -u $CLOUD_USER mkdir -p "$DST_PATH"
 
 pushd /srv
 tar cvf - minecraft/ | gzip -9 > $TMP_DIR/$FILENAME
 popd
-sudo chown $DROPBOX_USER:$DROPBOX_USER $TMP_DIR/$FILENAME
-mv -f $TMP_DIR/$FILENAME $DST_PATH
+sudo chown -R $CLOUD_USER:$CLOUD_USER $TMP_DIR
+sudo -u $CLOUD_USER mv -f $TMP_DIR/$FILENAME "$DST_PATH"
 
 rm -rf $TMP_DIR
 
-sudo -u $DROPBOX_USER $DROPBOX_WAITER
+sudo -u $CLOUD_USER "$CLOUD_WAITER"
